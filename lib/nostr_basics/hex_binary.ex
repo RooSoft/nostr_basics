5bc571b0275ec491bf2c2dc24a0684f29ebbf889
marc@roosoft.com <marc@roosoft.com>
2023-02-07 14:44:31 -0500

    doctests for HexBinary

--- lib/nostr_basics/hex_binary.ex
+++ lib/nostr_basics/hex_binary.ex
@@ -5,18 +5,27 @@ defmodule NostrBasics.HexBinary do
 
   Example:
 
+    ```elixir
     <<0x181DCB172F5B56DB39B6C8544068FEE69928556230B41E115BDB22BF82A06FF3::256>>
+    ```
 
     instead of
 
+    ```elixir
     <<24, 29, 203, 23, 47, 91, 86, 219, 57, 182, 200, 84, 64, 104, 254, 230, 153, 40, 85, 98, 48, 180, 30, 17, 91, 219, 34, 191, 130, 160, 111, 243>>
+    ```
   """
   defstruct [:data]
 
   alias NostrBasics.HexBinary
 
   @doc """
-  Constructor
+  Converts a binary string into a %HexBinary{} that the inspector will print in a compact way
+
+  ## Examples
+      iex> <<24, 29, 203, 23, 47, 91, 86, 219, 57, 182, 200, 84, 64, 104, 254, 230, 153, 40, 85, 98, 48, 180, 30, 17, 91, 219, 34, 191, 130, 160, 111, 243>>
+      ...> |> NostrBasics.HexBinary.from_binary()
+      %HexBinary{data: <<0x181DCB172F5B56DB39B6C8544068FEE69928556230B41E115BDB22BF82A06FF3::256>>}
   """
   def from_binary(data) when is_binary(data) do
     %HexBinary{data: data}
---
+++ test/nostr_basics/hex_binary_test.exs
@@ -0,0 +1,7 @@
+defmodule NostrBasics.HexBinaryTest do
+  use ExUnit.Case, async: true
+
+  alias NostrBasics.HexBinary
+
+  doctest HexBinary
+end
