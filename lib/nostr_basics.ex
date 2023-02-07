abbb658553e6f87e2722edb9faecd0544b79e4b6
marc@roosoft.com <marc@roosoft.com>
2023-02-07 14:49:09 -0500

    added a little more documentation on the hexdocs landing page

--- lib/nostr_basics.ex
+++ lib/nostr_basics.ex
@@ -1,5 +1,19 @@
 defmodule NostrBasics do
   @moduledoc """
   Basic structures both useful for nostr relays and clients
+
+  Takes care of:
+
+  - Cryptography
+    - SHA256
+    - Schnorr Signatures
+  - Events
+    - Parsing
+    - Serialization
+    - Signing
+    - Validation
+  - Filters
+    - Parsing
+    - Serialization
   """
 end
