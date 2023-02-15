230913c68b931834539395cfd3fdc8aeb5f51f4b
marc@roosoft.com <marc@roosoft.com>
2023-02-15 09:21:39 -0500

    test damus second request in ClientMessage

--- test/nostr_basics/client_message_test.exs
+++ test/nostr_basics/client_message_test.exs
@@ -15,8 +15,6 @@ defmodule NostrBasics.ClientMessageTest do
             [
               %NostrBasics.Filter{
                 subscription_id: "282C494A-BEB6-40C7-AD43-A33298BE077F",
-                since: nil,
-                until: nil,
                 limit: 500,
                 ids: [],
                 authors: [
@@ -29,5 +27,50 @@ defmodule NostrBasics.ClientMessageTest do
                 p: []
               }
             ]} == result
+  end
+
+  test "damus second request" do
+    req =
+      ~s(["REQ","E9D809BC-4511-4D19-A099-BC98600E7506",{"kinds":[0],"authors":["bdbe1bdbc9b25a8d89d8fdaf0be1a0dcd837bac9691f597892903a5fdd86e27f","32e1827635450ebb3c5a7d12c1f8e7b2b514439ac10a67eef3d9fd9c5c68e245","5ab9f2efb1fda6bc32696f6f3fd715e156346175b93b6382099d23627693c3f2"]},{"kinds":[3,0],"authors":["5ab9f2efb1fda6bc32696f6f3fd715e156346175b93b6382099d23627693c3f2"]},{"kinds":[30000],"authors":["5ab9f2efb1fda6bc32696f6f3fd715e156346175b93b6382099d23627693c3f2"],"#d":["mute"]}])
+
+    result = ClientMessage.parse(req)
+
+    assert {
+             :req,
+             [
+               %NostrBasics.Filter{
+                 subscription_id: "E9D809BC-4511-4D19-A099-BC98600E7506",
+                 ids: [],
+                 authors: [
+                   <<0xBDBE1BDBC9B25A8D89D8FDAF0BE1A0DCD837BAC9691F597892903A5FDD86E27F::256>>,
+                   <<0x32E1827635450EBB3C5A7D12C1F8E7B2B514439AC10A67EEF3D9FD9C5C68E245::256>>,
+                   <<0x5AB9F2EFB1FDA6BC32696F6F3FD715E156346175B93B6382099D23627693C3F2::256>>
+                 ],
+                 kinds: [0],
+                 e: [],
+                 p: []
+               },
+               %NostrBasics.Filter{
+                 subscription_id: "E9D809BC-4511-4D19-A099-BC98600E7506",
+                 ids: [],
+                 authors: [
+                   <<0x5AB9F2EFB1FDA6BC32696F6F3FD715E156346175B93B6382099D23627693C3F2::256>>
+                 ],
+                 kinds: [3, 0],
+                 e: [],
+                 p: []
+               },
+               %NostrBasics.Filter{
+                 subscription_id: "E9D809BC-4511-4D19-A099-BC98600E7506",
+                 ids: [],
+                 authors: [
+                   <<0x5AB9F2EFB1FDA6BC32696F6F3FD715E156346175B93B6382099D23627693C3F2::256>>
+                 ],
+                 kinds: [30000],
+                 e: [],
+                 p: []
+               }
+             ]
+           } == result
   end
 end
