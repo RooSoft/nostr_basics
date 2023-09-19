8c325f941e35c730f9ae6decf9ccbf8fd7159323
Marc Lacoursiere <marc@roosoft.com>
2023-09-19 09:41:18 -0400

    fixed a profile model test

--- lib/nostr_basics/models/profile.ex
+++ lib/nostr_basics/models/profile.ex
@@ -46,7 +46,7 @@ defmodule NostrBasics.Models.Profile do
           pubkey: <<0x5ab9f2efb1fda6bc32696f6f3fd715e156346175b93b6382099d23627693c3f2::256>>,
           kind: 0,
           tags: [],
-          content: ~s({"about":"some user description","banner":"https://image.com/satoshi_banner","display_name":"satoshi nakamoto","lud16":"satoshi@nakamoto.jp","name":"satoshi nakamoto","nip05":"_@nakamoto.jp","picture":"https://image.com/satoshi_avatar","website":"https://bitcoin.org"})
+          content: ~s({"name":"satoshi nakamoto","about":"some user description","banner":"https://image.com/satoshi_banner","display_name":"satoshi nakamoto","lud16":"satoshi@nakamoto.jp","nip05":"_@nakamoto.jp","picture":"https://image.com/satoshi_avatar","website":"https://bitcoin.org"})
         }
       }
   """
