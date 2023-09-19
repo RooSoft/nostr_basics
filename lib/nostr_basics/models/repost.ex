863ff914fced1557167c067af2aa5d30d9616860
Marc Lacoursiere <marc@roosoft.com>
2023-09-19 10:17:17 -0400

    fixed a repost test

--- lib/nostr_basics/models/repost.ex
+++ lib/nostr_basics/models/repost.ex
@@ -34,14 +34,15 @@ defmodule NostrBasics.Models.Repost do
       ...> |> NostrBasics.Models.Repost.to_event(<<0x5ab9f2efb1fda6bc32696f6f3fd715e156346175b93b6382099d23627693c3f2::256>>)
       {
         :ok,
-       %NostrBasics.Event{
-         pubkey: <<0x5ab9f2efb1fda6bc32696f6f3fd715e156346175b93b6382099d23627693c3f2::256>>, 
-         kind: 6, 
-         tags: [
-           ["e", "012b55abd446d7f4e4abc5b2570ce61c3e289f5683128fd469dd2016401234e4"], 
-           ["p", "7f3b464b9ff3623630485060cbda3a7790131c5339a7803bde8feb79a5e1b06a"]
-         ], 
-         content: ~s({"id":"012b55abd446d7f4e4abc5b2570ce61c3e289f5683128fd469dd2016401234e4","sig":"4dd5e4718dcc854e57d3ab37b1f2c515f1a13beeeabc1a7c93c338510c26407a86ccd9f38ebcb97389ca875307ac05ee0784dbbe09c1cc000cf707157c723847","kind":1,"relays":["wss://nos.lol"],"pubkey":"7f3b464b9ff3623630485060cbda3a7790131c5339a7803bde8feb79a5e1b06a","tags":[["e","57d70d162b28c66b152c5c99d0b73490b5362b93baf7746e0715f5221d0e18aa"],["e","771eeb8a06fbff0e416a9c27da152d52f594e540dd7b09b4d512d5e488377f73"],["p","dbe0605a9c73172bad7523a327b236d55ea4b634e80e78a9013db791f8fd5b2c"],["p","a9b9525992a486aa16b3c1d3f9d3604bca08f3c15b712d70711b9aecd8c3dc44"]],"content":"I just zapped you. My first zap!","created_at":1676335417}), sig: nil}
+        %NostrBasics.Event{
+          id: nil, 
+          pubkey: <<0x5ab9f2efb1fda6bc32696f6f3fd715e156346175b93b6382099d23627693c3f2::256>>, 
+          created_at: nil, 
+          kind: 6, 
+          tags: [
+            ["e", "012b55abd446d7f4e4abc5b2570ce61c3e289f5683128fd469dd2016401234e4"], 
+            ["p", "7f3b464b9ff3623630485060cbda3a7790131c5339a7803bde8feb79a5e1b06a"]], 
+            content: ~s({"id":"012b55abd446d7f4e4abc5b2570ce61c3e289f5683128fd469dd2016401234e4","sig":"4dd5e4718dcc854e57d3ab37b1f2c515f1a13beeeabc1a7c93c338510c26407a86ccd9f38ebcb97389ca875307ac05ee0784dbbe09c1cc000cf707157c723847","kind":1,"relays":["wss://nos.lol"],"pubkey":"7f3b464b9ff3623630485060cbda3a7790131c5339a7803bde8feb79a5e1b06a","tags":[["e","57d70d162b28c66b152c5c99d0b73490b5362b93baf7746e0715f5221d0e18aa"],["e","771eeb8a06fbff0e416a9c27da152d52f594e540dd7b09b4d512d5e488377f73"],["p","dbe0605a9c73172bad7523a327b236d55ea4b634e80e78a9013db791f8fd5b2c"],["p","a9b9525992a486aa16b3c1d3f9d3604bca08f3c15b712d70711b9aecd8c3dc44"]],"content":"I just zapped you. My first zap!","created_at":1676335417}), sig: nil}
       }
   """
   @spec to_event(Repost.t(), PublicKey.id()) :: {:ok, Event.t()} | {:error, String.t()}
