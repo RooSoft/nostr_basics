2f85d53d577d89bf41a3b92f1236720035efae7d
Marc Lacoursiere <marc@roosoft.com>
2023-09-21 09:42:46 -0400

    updated dialyxir to 1.4

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule NostrBasics.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.29.1", only: [:dev], runtime: false},
-      {:dialyxir, "~> 1.2", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:jason, "~> 1.4"},
       {:k256, "~> 0.0.8"},
